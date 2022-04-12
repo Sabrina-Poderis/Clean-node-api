a444d3ac86dd0d0b1adedfe73ba87172239321b3
Sabrina Poderis <sabrina.poderis@gmail.com>
2022-04-12 18:01:26 -0300

    ensure LoginRouter returns 401 when invalid credentials are provided

--- src/presentation/router/login-router.js
+++ src/presentation/router/login-router.js
@@ -18,5 +18,9 @@ module.exports = class LoginRouter {
       return HttpReponse.badRequest('password')
     }
     this.authUseCase.auth(email, password)
+
+    return {
+      statusCode: 401
+    }
   }
 }
