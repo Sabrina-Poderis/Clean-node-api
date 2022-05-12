ea9eaefed35bffc57f3d0d6deedc421f1e87527a
Sabrina-Poderis <sabrina.poderis@gmail.com>
2022-05-12 15:59:20 -0300

    ensure AuthUseCase returns an accessToken if valid credentials are provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -109,4 +109,10 @@ describe('AuthUseCase', () => {
     await sut.auth('valid_email@email.com', 'valid_password')
     expect(tokenGeneratorSpy.userId).toBe(loadUserByEmailRepositorySpy.user.id)
   })
+  test('Should returnan an accessToken if correct credentials are provided', async () => {
+    const { sut, tokenGeneratorSpy } = makeSut()
+    const accessToken = await sut.auth('valid_email@email.com', 'valid_password')
+    expect(accessToken).toBe(tokenGeneratorSpy.accessToken)
+    expect(accessToken).toBeTruthy()
+  })
 })
